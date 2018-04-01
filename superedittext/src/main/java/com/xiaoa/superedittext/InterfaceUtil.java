ee8850aeb8aad8dd5d5b3bd338120049bffe6b7b
xiaoa <lcupupup@163.com>
2018-04-02 01:29:30 +0800

    第一次提交
    第一次提交

---
+++ superedittext/src/main/java/com/xiaoa/superedittext/InterfaceUtil.java
@@ -0,0 +1,14 @@
+package com.xiaoa.superedittext;
+
+/**
+ * Created by 40645 on 2018/4/2.
+ */
+
+public class InterfaceUtil {
+
+    public  interface  TextOnchanged{
+        void  onChanged();
+
+    }
+
+}
